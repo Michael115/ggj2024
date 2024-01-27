940bfac25d814c49a101c69c0c821d5501cb853d
Michael <michael@makarness.com>
2024-01-27 22:16:04 +0000

    add a shotgun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -14,6 +14,8 @@ public class Gun : MonoBehaviour
 {
     public float rpm;
     public float damage;
+    public int numberOfBullets = 1;
+    public int numberOfShells = 1;
     public GunFireMode gunFireMode;
     public Transform shootPoint;
     public Transform shellEjectionPoint;
@@ -54,22 +56,29 @@ public class Gun : MonoBehaviour
             shootSound.PlayOneShot(shootSound.clip);
         }
 
-        Rigidbody shellCase = Instantiate(shell, shellEjectionPoint.position, Quaternion.identity);
-        shellCase.AddForce(shellEjectionPoint.forward * Random.Range(ejectForce * 0.5f, ejectForce * 1.5f) +
-                           shootPoint.forward * Random.Range(-ejectForce * 0.1f, ejectForce * 0.1f));
-
-        var direction = shootPoint.forward;
-        direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.up) * direction;
-        direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.right) * direction;
-        direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.down) * direction;
-        direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.left) * direction;
-
-        var nextBullet = Instantiate(bullet, shootPoint.position,
-            Quaternion.LookRotation(shootPoint.forward, shootPoint.transform.up));
-        var rb = nextBullet.GetComponent<Rigidbody>();
-        rb.AddForce((direction * bulletSpeed), ForceMode.VelocityChange);
-        nextBullet.Damage = damage;
-
+        for (int i = 0; i < numberOfShells; i++)
+        {
+            Rigidbody shellCase = Instantiate(shell, shellEjectionPoint.position, Quaternion.identity);
+            shellCase.AddForce(shellEjectionPoint.forward * Random.Range(ejectForce * 0.5f, ejectForce * 1.5f) +
+                               shootPoint.forward * Random.Range(-ejectForce * 0.1f, ejectForce * 0.1f));
+        }
+        
+        
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            var direction = shootPoint.forward;
+            direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.up) * direction;
+            direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.right) * direction;
+            direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.down) * direction;
+            direction = Quaternion.AngleAxis(Random.Range(0, accuracyPenalty), Vector3.left) * direction;
+            
+            var nextBullet = Instantiate(bullet, shootPoint.position,
+                Quaternion.LookRotation(direction.normalized, shootPoint.transform.up));
+            var rb = nextBullet.GetComponent<Rigidbody>();
+            rb.AddForce((direction * bulletSpeed), ForceMode.VelocityChange);
+            nextBullet.Damage = damage;
+        }
+     
         shootEffect.Play();
 
         muzzleLight.enabled = true;
