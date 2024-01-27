88fe6e6540ef9e6024d6689ffc67f00fd500976c
Michael <michael@makarness.com>
2024-01-27 20:01:04 +0000

    working first version

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,8 +5,10 @@ using UnityEngine.VFX;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private VisualEffect impactEffect;
+    [SerializeField] private VisualEffect impactEffectEnemy;
 
     private bool _hasImpactEffect;
+    private bool _hasImpactEffectEnemy;
 
     // set by gun
     internal float Damage;
@@ -14,6 +16,7 @@ public class Bullet : MonoBehaviour
     private void Awake()
     {
         _hasImpactEffect = impactEffect != null;
+        _hasImpactEffectEnemy = impactEffectEnemy != null;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -22,12 +25,27 @@ public class Bullet : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out Health health))
         {
             health.ApplyDamage(Damage);
+            HitEffectEnemy(collision.GetContact(0).point);
         }
-
-        HitEffect(collision.GetContact(0).point);
+        else
+        {
+            HitEffect(collision.GetContact(0).point);
+        }
+       
         Destroy(gameObject);
     }
 
+    private void HitEffectEnemy(Vector3 collisionPoint)
+    {
+        if (_hasImpactEffectEnemy)
+        {
+            impactEffectEnemy.transform.position = collisionPoint + transform.forward * -0.1f;
+            impactEffectEnemy.transform.SetParent(null);
+            impactEffectEnemy.Play();
+            impactEffectEnemy.AddComponent<TimedDestroy>().duration = 25;
+        }
+    }
+    
     private void HitEffect(Vector3 collisionPoint)
     {
         if (_hasImpactEffect)
