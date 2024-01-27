8015617b70a1a11b445cefa6ab8a6886baf76a89
David Strachan <davidstrachan92@gmail.com>
2024-01-27 12:50:26 +0000

    Add NavMesh sample scene

---
+++ Assets/Scripts/NavMeshEnemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshEnemy : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.destination = player.position;
+    }
+}
