d4721f58795a18528986a5091d82e1b1675bf17a
David Strachan <davidstrachan92@gmail.com>
2024-01-28 13:23:51 +0000

    Add enemy tickle laughter

--- Assets/Scripts/LaughterMeter.cs
+++ Assets/Scripts/LaughterMeter.cs
@@ -7,16 +7,42 @@ public class LaughterMeter : MonoBehaviour
     [Range(0, 100)] public float percentage;
     [SerializeField] private float decayPerSecond;
     [SerializeField] private Slider laughterSlider;
+    [SerializeField] private float damage;
+    [SerializeField] private float damageIntervalInSeconds;
+
+    private float _nextDamageTime;
 
     private void Update()
     {
         var decay = Time.deltaTime * decayPerSecond;
         percentage = Math.Max(0, percentage - decay);
         laughterSlider.value = percentage;
+    }
 
+    private void Tickle(float tickleAmount)
+    {
+        _nextDamageTime = Time.time + damageIntervalInSeconds;
+        percentage = Math.Min(100, percentage + tickleAmount);
+        laughterSlider.value = percentage;
         if (percentage >= 100)
         {
             // TODO: End game.
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && Time.time >= _nextDamageTime)
+        {
+            Tickle(damage);
+        }
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy") && Time.time >= _nextDamageTime)
+        {
+            Tickle(damage);
         }
     }
 }
