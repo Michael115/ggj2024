442bcb0d43ca3f5c7f62b4de5110d6fa214c5087
David Strachan <davidstrachan92@gmail.com>
2024-01-29 15:30:16 +0000

    Only show button when box is not open

--- Assets/Scripts/ShopCrate.cs
+++ Assets/Scripts/ShopCrate.cs
@@ -17,15 +17,15 @@ public class ShopCrate : MonoBehaviour
     public bool gunReady = false;
 
     public bool denied;
-    
+
     public int cost = 800;
-    
+
     private PlayerController _player;
     private InputSystemReader _inputReader;
     private Gun _randomGun;
 
-   [SerializeField] private GameObject openButton;
-    
+    [SerializeField] private GameObject openButton;
+
     private void Start()
     {
         _openLightColor = openLight.color;
@@ -41,7 +41,7 @@ public class ShopCrate : MonoBehaviour
             _inputReader.InteractEvent += OnInteract;
         }
     }
-    
+
     void OnDisable()
     {
         _inputReader ??= GameController.Instance.InputReader;
@@ -50,18 +50,19 @@ public class ShopCrate : MonoBehaviour
             _inputReader.InteractEvent -= OnInteract;
         }
     }
-    
+
     private void OnInteract()
-    {      
+    {
         if (!playerCanInteract) return;
         if (denied) return;
-        
+
         if (!boxOpen && _player.playerMoney < 800)
         {
             denied = true;
             StartCoroutine(RedLight());
             return;
         }
+
         if (boxOpen && gunReady)
         {
             // Select Gun
@@ -70,16 +71,18 @@ public class ShopCrate : MonoBehaviour
             gunReady = false;
             return;
         }
+
         if (!boxOpen)
         {
             _player.AddMoney(-800);
             boxOpen = true;
+            openButton.SetActive(false);
             animator.Play("Open");
             StartCoroutine(DelayShow());
             return;
         }
     }
-    
+
     IEnumerator RedLight()
     {
         openLight.color = Color.red;
@@ -87,35 +90,36 @@ public class ShopCrate : MonoBehaviour
         openLight.color = _openLightColor;
         denied = false;
     }
-    
+
     IEnumerator DelayShow()
-    {  
+    {
         yield return new WaitForSeconds(0.5f);
-        _randomGun = _guns[Random.Range(0, _guns.Length-1)];
+        _randomGun = _guns[Random.Range(0, _guns.Length - 1)];
         for (int i = 10; i < 30; i++)
         {
-            yield return new WaitForSeconds(i*0.01f);
+            yield return new WaitForSeconds(i * 0.01f);
             foreach (var gun in _guns)
             {
                 gun.gameObject.SetActive(false);
             }
-            _randomGun = _guns.Where(w=>w != _randomGun).ToList()[Random.Range(0, _guns.Length-1)];
+
+            _randomGun = _guns.Where(w => w != _randomGun).ToList()[Random.Range(0, _guns.Length - 1)];
             _randomGun.gameObject.SetActive(true);
         }
 
         gunReady = true;
-        
+
         yield return new WaitForSeconds(3f);
-        
+
         _randomGun.gameObject.SetActive(false);
         gunReady = false;
         animator.Play("Close");
-        
+
         yield return new WaitForSeconds(1f);
         openLight.enabled = false;
         // Cooldown
         yield return new WaitForSeconds(1f);
-        
+
         openLight.enabled = true;
         boxOpen = false;
     }
@@ -134,7 +138,10 @@ public class ShopCrate : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerCanInteract = true;
-            openButton.SetActive(true);
+            if (!boxOpen)
+            {
+                openButton.SetActive(true);
+            }
         }
     }
 }
