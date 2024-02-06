5f92ad01190531787a73fb1ffd726e2f59f77ff5
Craig Innes <craiginnes@gmail.com>
2024-02-06 18:48:18 +0000

    Ammo Display Shakes when ammo is low

--- Assets/AmmoDisplay.cs
+++ Assets/AmmoDisplay.cs
@@ -10,18 +10,42 @@ public class AmmoDisplay : MonoBehaviour
 
     private PlayerController player;
     private TextMeshProUGUI uiText;
+    private Vector3 originalPos;
+
+    public float shakeAmpMax;
+    public float shakeSpeedMax;
+
+    public float lowAmmoThreshold;
+
+    public Color fullAmmoColor;
+    public Color lowAmmoColor;
     
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         uiText = GetComponent<TextMeshProUGUI>();
+        originalPos = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         var equippedGun = player.GetPlayerGun();
+
+        float ammo_prop = equippedGun.GetCurrentAmmo() / (float) equippedGun.maxAmmo;
         uiText.text = equippedGun.GetCurrentAmmo() + "/" + equippedGun.maxAmmo;
+
+        if (ammo_prop < lowAmmoThreshold)
+        {
+            uiText.color = lowAmmoColor;
+            transform.localPosition = originalPos + shakeAmpMax * Mathf.Sin(Time.time * shakeSpeedMax) * Vector3.left;
+        }
+        else
+        {
+            uiText.color = fullAmmoColor;
+            transform.localPosition = originalPos;
+        }
+        
 
     }
 }
